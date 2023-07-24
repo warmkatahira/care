e28e691fe90cd9f62e61c35bf3e6d0d0951d821a
warmkatahira <t.katahira@warm.co.jp>
2023-07-24 17:30:26 +0900

    20230724_1

--- resources/views/meal/index.blade.php
+++ resources/views/meal/index.blade.php
@@ -1,22 +1,22 @@
 <x-app-layout>
-    <div class="mt-1 xl:mt-2 mb-5">
+    <div class="mt-1 xl:mt-2 mb-5 px-0.5">
         <div class="flex flex-row">
             <a href="{{ route('meal_create.index') }}" class="CherryBombOne text-sm xl:text-2xl text-theme-third px-5 bg-theme-sub"><i class="las la-plus-square mr-1"></i>とうろく</a>
             <a href="{{ route('meal.index', ['month' => $month_change['prev']]) }}" class="text-sm xl:text-xl text-theme-third px-5 bg-theme-sub py-0.5 ml-auto"><i class="las la-caret-square-left la-lg"></i>{{ \Carbon\CarbonImmutable::parse($month_change['prev'])->isoFormat('YYYY年MM月') }}</a>
             <a href="{{ route('meal.index', ['month' => $month_change['next']]) }}" class="text-sm xl:text-xl text-theme-third px-5 bg-theme-sub py-0.5 ml-5 mr-0.5">{{ \Carbon\CarbonImmutable::parse($month_change['next'])->isoFormat('YYYY年MM月') }}<i class="las la-caret-square-right la-lg"></i></a>
         </div>
-        <div class="grid grid-cols-7 mb-0.5 mt-1 xl:mt-2">
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">日</p>
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">月</p>
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">火</p>
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">水</p>
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">木</p>
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">金</p>
-            <p class="col-span-1 bg-theme-third border border-theme-sub text-center mr-0.5">土</p>
+        <div class="grid grid-cols-7 gap-x-0.5 mb-0.5 mt-1 xl:mt-2">
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">日</p>
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">月</p>
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">火</p>
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">水</p>
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">木</p>
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">金</p>
+            <p class="col-span-1 bg-theme-third border border-theme-sub text-center">土</p>
         </div>
-        <div class="grid grid-cols-7">
+        <div class="grid grid-cols-7 gap-x-0.5">
             @foreach($date_arr as $key => $date)
-                <div class="col-span-1 px-1 pt-1 bg-theme-third border border-theme-sub mr-0.5 mb-0.5 h-20">
+                <div class="col-span-1 px-1 pt-1 bg-theme-third border border-theme-sub mb-0.5 h-20">
                     <p class="text-10px xl:text-sm">{{ $key }}</p>
                     @if(!empty($date))
                         @foreach($date as $item)
